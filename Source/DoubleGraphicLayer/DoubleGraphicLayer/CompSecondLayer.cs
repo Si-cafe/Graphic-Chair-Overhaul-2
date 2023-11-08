671cb88eace042388c7f808d94d1b75ffe66a747
SebChmie <sebchmie@freenet.de>
2023-11-08 19:31:19 +0100

    Add Harmony patches

--- Source/DoubleGraphicLayer/DoubleGraphicLayer/CompSecondLayer.cs
+++ Source/DoubleGraphicLayer/DoubleGraphicLayer/CompSecondLayer.cs
@@ -1,5 +1,4 @@
 ﻿using RimWorld;
-using System;
 using System.Collections.Generic;
 using UnityEngine;
 using Verse;
@@ -7,28 +6,46 @@ using Verse.Sound;
 
 namespace DoubleGraphicLayer
 {
+    public class CompProperties_SecondLayer : CompProperties
+    {
+        public GraphicData graphicData = null;
+        public AltitudeLayer altitudeLayer = AltitudeLayer.PawnState;
+
+        public bool allwaysShowGizmo = false;
+
+        public float Altitude => Altitudes.AltitudeFor(altitudeLayer);
+
+        public CompProperties_SecondLayer() => compClass = typeof(CompSecondLayer);
+    }
+
     public class CompSecondLayer : ThingComp
     {
+        private static readonly SecondLayerSettings Settings = LoadedModManager.GetMod<SecondLayerMod>().GetSettings<SecondLayerSettings>();
+
         private Graphic graphicInt;
-        public bool toggleSecondLayer = LoadedModManager.GetMod<SecondLayerMod>().GetSettings<SecondLayerSettings>().defaultToggle;
-        public Graphic_Multi secondLayer;
-        public Graphic_Single secondLayerSingle;
+        public bool toggleSecondLayer = Settings.defaultToggle;
 
-        private string defaultLayerPath;
-        private string styleLayerPath;
+        private bool ShowSecondLayer => parent.Rotation == Rot4.North || Props.allwaysShowGizmo;
 
-        public CompProperties_SecondLayer Props
+        public CompProperties_SecondLayer Props => (CompProperties_SecondLayer)props;
+
+        // Mod "Faster Game Loading" delays the loading of graphics (optional), so graphicInt might be null
+        public virtual Graphic Graphic
         {
             get
             {
-                return (CompProperties_SecondLayer)this.props;
+                if (graphicInt == null) SecondLayerAssign();
+                return graphicInt ?? BaseContent.BadGraphic;
             }
         }
+
         public override void PostSpawnSetup(bool respawningAfterLoad)
         {
             base.PostSpawnSetup(respawningAfterLoad);
-            GraphicData propsData = this.Props.graphicData;
-            GraphicData parentData = this.parent.def.graphicData;
+
+            var propsData = Props.graphicData;
+            var parentData = parent.def.graphicData;
+
             propsData.drawSize = parentData.drawSize;
             propsData.graphicClass = parentData.graphicClass;
             propsData.shaderType = parentData.shaderType;
@@ -37,128 +54,86 @@ namespace DoubleGraphicLayer
             propsData.drawOffsetEast = parentData.drawOffsetEast;
             propsData.drawOffsetSouth = parentData.drawOffsetSouth;
             propsData.drawOffsetWest = parentData.drawOffsetWest;
-            LongEventHandler.ExecuteWhenFinished(delegate { SecondLayerAssign(); });
+
+            LongEventHandlerSecondLayerAssign();
         }
+
+        public override void PostDraw()
+        {
+            if (toggleSecondLayer && ShowSecondLayer)
+            {
+                base.PostDraw();
+                var loc = GenThing.TrueCenter(parent.Position, parent.Rotation, parent.def.size, Props.Altitude);
+                Graphic.Draw(loc, parent.Rotation, parent);
+            }
+        }
+
+        public override void PostExposeData() => Scribe_Values.Look(ref toggleSecondLayer, "toggleSecondLayer", true);
+
+        public override IEnumerable<Gizmo> CompGetGizmosExtra()
+        {
+            foreach (var gizmo in base.CompGetGizmosExtra()) yield return gizmo;
+
+            if (ShowSecondLayer)
+            {
+                if (Settings.showGizmo) yield return new Command_Toggle
+                {
+                    defaultLabel = "DoubleGraphicLayer_Toggle".Translate(),
+                    defaultDesc = "DoubleGraphicLayer_ToggleDesc".Translate(),
+
+                    icon = ContentFinder<Texture2D>.Get("UI/DGL_Toggle", true),
+                    isActive = () => toggleSecondLayer,
+                    toggleAction = () => toggleSecondLayer = !toggleSecondLayer
+                };
+                // If Harmony is used, the patches should make this unnecessary 
+                if (Graphic.Color != parent.Graphic.Color || (Graphic.path != $"{parent.Graphic.path}_back")) yield return new Command_Action
+                {
+                    defaultLabel = "DoubleGraphicLayer_Update".Translate(),
+                    defaultDesc = "DoubleGraphicLayer_UpdateDesc".Translate(),
+
+                    icon = ContentFinder<Texture2D>.Get("UI/DGL_Update", true),
+                    action = () =>
+                    {
+                        SoundDefOf.Designate_Paint.PlayOneShotOnCamera(null);
+                        LongEventHandlerSecondLayerAssign();
+                    }
+                };
+            }
+            yield break;
+        }
+
+        public void LongEventHandlerSecondLayerAssign() => LongEventHandler.ExecuteWhenFinished(SecondLayerAssign);
 
         public void SecondLayerAssign()
         {
             try
             {
-                ShaderTypeDef layerShader = this.parent.def.graphicData.shaderType;
-                Vector2 layerSize = this.parent.def.graphicData.drawSize;
-                Color layerColor = this.parent.Graphic.Color;
-                Color layerColotwo = this.parent.Graphic.ColorTwo;
-                GraphicData layerData = this.Props.graphicData;
-                ThingStyleDef styleDef = new ThingStyleDef();
-                Type graphicClass = Props.graphicData.graphicClass;
-                if (graphicClass == typeof(Graphic_Multi))
-                {
-                    if (styleDef != null && this.parent.StyleDef != null)
-                    {
-                        ShaderTypeDef styleShader = this.parent.StyleDef.graphicData.shaderType;
-                        Vector2 styleSize = this.parent.StyleDef.graphicData.drawSize;
-                        styleLayerPath = this.parent.StyleDef.graphicData.texPath + "_back";
-                        secondLayer = (Graphic_Multi)GraphicDatabase.Get<Graphic_Multi>(styleLayerPath, styleShader.Shader, styleSize, layerColor, layerColotwo, layerData);
-                    }
-                    else
-                    {
-                        defaultLayerPath = parent.Graphic.path + "_back";
-                        secondLayer = (Graphic_Multi)GraphicDatabase.Get<Graphic_Multi>(defaultLayerPath, layerShader.Shader, layerSize, layerColor, layerColotwo, layerData);
-                    }
-                    this.graphicInt = secondLayer;
-                }
-                else if (graphicClass == typeof(Graphic_Single))
-                {
-                    if (styleDef != null && this.parent.StyleDef != null)
-                    {
-                        ShaderTypeDef styleShader = this.parent.StyleDef.graphicData.shaderType;
-                        Vector2 styleSize = this.parent.StyleDef.graphicData.drawSize;
-                        styleLayerPath = this.parent.StyleDef.graphicData.texPath + "_back";
-                        secondLayerSingle = (Graphic_Single)GraphicDatabase.Get<Graphic_Single>(styleLayerPath, styleShader.Shader, styleSize, layerColor, layerColotwo, layerData);
-                    }
-                    else
-                    {
-                        defaultLayerPath = parent.Graphic.path + "_back";
-                        secondLayerSingle = (Graphic_Single)GraphicDatabase.Get<Graphic_Single>(defaultLayerPath, layerShader.Shader, layerSize, layerColor, layerColotwo, layerData);
-                    }
-                    this.graphicInt = secondLayerSingle;
-                }
-            } 
-            catch (Exception)
-            {
-                Log.Message("::Graphic Chair Overhaul => Not applicable to " + this.parent.def.defName + ".");
+                var graphicClass = Props.graphicData.graphicClass;
+
+                if (graphicClass == typeof(Graphic_Multi)) SecondLayerAssignInt<Graphic_Multi>();
+                else if (graphicClass == typeof(Graphic_Single)) SecondLayerAssignInt<Graphic_Multi>();
             }
-        }
-        public virtual Graphic Graphic
-        {
-            get
+            catch (System.Exception)
             {
-                if (this.graphicInt == null)
-                {
-                    return BaseContent.BadGraphic;
-                }
-                return this.graphicInt;
+                // Log.Error() helps in debugging, but it's not necessary
+                Log.Error($"::Graphic Chair Overhaul => Not applicable to {parent.def.defName}.");
             }
-        }
-        public override void PostDraw()
-        {
-            bool flag1 = toggleSecondLayer;
-            bool flag2 = this.parent.Rotation == Rot4.North || this.Props.allwaysShowGizmo;
-            if (flag1 && flag2)
-            {
-                base.PostDraw();
-                Vector3 loc = GenThing.TrueCenter(this.parent.Position, this.parent.Rotation, this.parent.def.size, this.Props.Altitude);
-                this.Graphic.Draw(loc, this.parent.Rotation, this.parent);
-            }
-        }
-        public override void PostExposeData()
-        {
-            Scribe_Values.Look<bool>(ref this.toggleSecondLayer, "toggleSecondLayer", true);
         }
 
-        public override IEnumerable<Gizmo> CompGetGizmosExtra()
+        private void SecondLayerAssignInt<G>() where G : Graphic, new()
         {
-            foreach (Gizmo gizmo in base.CompGetGizmosExtra())
-            {
-                yield return gizmo;
-            }
-            bool flag3 = this.parent.Rotation == Rot4.North || this.Props.allwaysShowGizmo == true;         
-            if (flag3)
-            {
-                bool flag4 = LoadedModManager.GetMod<SecondLayerMod>().GetSettings<SecondLayerSettings>().showGizmo;
-                bool flag5 = this.Graphic.Color != this.parent.Graphic.Color || (this.Graphic.path != this.parent.Graphic.path + "_back");
-                if (flag4)
-                {
-                    yield return new Command_Toggle
-                    {
-                        defaultLabel = "DoubleGraphicLayer_Toggle".Translate(),
-                        defaultDesc = "DoubleGraphicLayer_ToggleDesc".Translate(),
+            var graphic = parent.Graphic;
+            var data = parent.StyleDef?.graphicData;
 
-                        icon = ContentFinder<Texture2D>.Get("UI/DGL_Toggle", true),
-                        isActive = (() => this.toggleSecondLayer),
-                        toggleAction = delegate ()
-                        {
-                            this.toggleSecondLayer = !this.toggleSecondLayer;
-                        }
-                    };
-                }
-                if (flag5)
-                {
-                    yield return new Command_Action
-                    {
-                        defaultLabel = "DoubleGraphicLayer_Update".Translate(),
-                        defaultDesc = "DoubleGraphicLayer_UpdateDesc".Translate(),
+            var path = $"{data?.texPath ?? graphic.path}_back";
+            data ??= parent.def.graphicData;
 
-                        icon = ContentFinder<Texture2D>.Get("UI/DGL_Update", true),
-                        action = delegate ()
-                        {
-                            SoundDefOf.Designate_Paint.PlayOneShotOnCamera(null);
-                            LongEventHandler.ExecuteWhenFinished(delegate { SecondLayerAssign(); });
-                        }
-                    };
-                }
-            }
-            yield break;
+            var shaderType = data.shaderType;
+
+            // Mod "Faster Game Loading" delays the loading of graphics (optional), so shaderType might be null
+            if (shaderType == null) return;
+
+            graphicInt = GraphicDatabase.Get<G>(path, shaderType.Shader, data.drawSize, graphic.Color, graphic.ColorTwo, Props.graphicData);
         }
     }
 }
---
+++ Source/DoubleGraphicLayer/DoubleGraphicLayer/DGLHarmony.cs
@@ -0,0 +1,49 @@
+﻿using RimWorld;
+using Verse;
+
+namespace DoubleGraphicLayer
+{
+    public class DGLHarmony : Mod
+    {
+        public static string harmID = "rimworld.DGL.harmony";
+
+        public DGLHarmony(ModContentPack content) : base(content)
+        {
+            // Check if Harmony is loaded
+            try
+            {
+                Patch();
+            }
+            catch
+            {
+            }
+        }
+
+        // Separate function for the check to work
+        private static void Patch()
+        {
+            HarmonyLib.Harmony harm = new(harmID);
+            harm.PatchAll();
+
+            var harmType = typeof(DGLHarmony);
+
+            // Reassign the second layer when the color changes
+            harm.Patch(
+                typeof(Thing).GetMethod(nameof(Thing.Notify_ColorChanged)),
+                postfix: new(harmType, nameof(Postfix_SecondLayerAssign_Color))
+            );
+
+            // Reassign the second layer when the style changes
+            harm.Patch(
+                typeof(ThingStyleHelper).GetMethod(nameof(ThingStyleHelper.SetStyleDef)),
+                postfix: new(harmType, nameof(Postfix_SecondLayerAssign_Style))
+            );
+        }
+
+        // the name of the Thing must match the signature of the patched method
+        private static void Postfix_SecondLayerAssign_Color(Thing __instance) => Postfix_SecondLayerAssign_Style(__instance);
+
+        private static void Postfix_SecondLayerAssign_Style(Thing thing) => thing.TryGetComp<CompSecondLayer>()?.LongEventHandlerSecondLayerAssign();
+    }
+}
+
